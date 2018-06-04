6f9ccd282a1e2b2b2cf7f79fb3a575ae1c14b16a
Rohit Awate <rohitawate121@gmail.com>
2018-06-04 16:42:40 +0530

    Improved BugReporter and moved it into its own separate module

--- src/main/java/com/rohitawate/everest/Main.java
+++ src/main/java/com/rohitawate/everest/Main.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.rohitawate.everest.main;
+package com.rohitawate.everest;
 
 import com.rohitawate.everest.util.EverestUtilities;
 import com.rohitawate.everest.util.Services;
